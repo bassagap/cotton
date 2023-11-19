e05f8893592352e3d2de1dd02416ec163f223510
paula.bassaganas@boehringer-ingelheim.com <paula.bassaganas@boehringer-ingelheim.com>
2023-11-19 12:31:48 +0100

    fix deploy folder

--- Infrastructure/backend.tf
+++ Infrastructure/backend.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "terraform.tfstate"
     region         = "eu-west-1"
     encrypt        = true
-    dynamodb_table = "terraform_locks"
+    dynamodb_table = "cotton-terraform-locks"
   }
 }
